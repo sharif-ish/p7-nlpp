e129fd0f354e3a7dcb0acce8e5a437f1002602a0
raselkhondoker <mrrasel922@gmail.com>
2020-03-28 19:05:59 +0600

    Initialized p7-nlp

---
+++ installation.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+source venv/bin/activate
+pip3 install flask
+pip3 install pyresparser
+python3 -m spacy download en_core_web_sm
+python3 -m nltk.downloader words
+python3 download_stopwords.py
