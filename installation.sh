09e7ec169ba541846795f4c74bdd348f2ae3410d
raselkhondoker <mrrasel922@gmail.com>
2020-03-29 10:09:18 +0600

    remove venv and update instalation.sh

--- installation.sh
+++ installation.sh
@@ -6,3 +6,4 @@ pip3 install pyresparser
 python3 -m spacy download en_core_web_sm
 python3 -m nltk.downloader words
 python3 download_stopwords.py
+cp resume_parser.py venv/lib/python3.6/site-packages/pyresparser/
