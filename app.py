596911719d2b93ad5b0c69fcf8e5a9c420caf513
raselkhondoker <mrrasel922@gmail.com>
2020-04-12 18:46:26 +0600

    Added UI

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from pyresparser import ResumeParser
 from config import METHOD, HOST, PORT
 from skills import custom_skills
@@ -10,7 +10,19 @@ def JDParser():
     req_json = request.json
     job_desc = req_json['desc']
     extracted_info = ResumeParser(job_desc, skills_file=custom_skills).get_extracted_data()
-    return jsonify({'Skills': extracted_info['skills']})
+    return jsonify({'all': extracted_info})
+
+@app.route('/ui')
+def home():
+    return render_template('index.html')
+
+@app.route('/ui', methods= METHOD)
+def extract():
+    job_desc = request.form['desc']
+    print(job_desc)
+    extracted_info = ResumeParser(job_desc, skills_file=custom_skills).get_extracted_data()
+    extracted_skill = extracted_info['skills']
+    return render_template('index.html', skills = extracted_skill)
 
 if __name__ == '__main__':
     app.run(host=HOST, port=PORT)
