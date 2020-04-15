458d4fcaed649e3b7319f2a9dcc2bc30eeba6467
raselkhondoker <mrrasel922@gmail.com>
2020-04-15 09:50:09 +0600

    Changing URL

--- app.py
+++ app.py
@@ -9,19 +9,19 @@ def parser(get_data):
     extracted_info = ResumeParser(get_data['desc'], skills_file=custom_skills).get_extracted_data()
     return extracted_info
 
-@app.route('/', methods= METHOD)
-def JDParser():
-    extracted_info = parser(request.json)
-    return jsonify({'all': extracted_info})
-
-@app.route('/skills')
+@app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/skills', methods= METHOD)
+@app.route('/', methods= METHOD)
 def extract():
     extracted_skill = parser(request.form)['skills']
     return render_template('index.html', skills = extracted_skill)
+
+@app.route('/api', methods= METHOD)
+def JDParser():
+    extracted_info = parser(request.json)
+    return jsonify({'all': extracted_info})
 
 if __name__ == '__main__':
     app.run(host=HOST, port=PORT)
