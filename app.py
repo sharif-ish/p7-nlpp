a2505b23e637ff119460a83b1a53328c509f616c
raselkhondoker <mrrasel922@gmail.com>
2020-04-26 11:58:54 +0600

    Changed UI Grid as form value stay same afer reload

--- app.py
+++ app.py
@@ -8,10 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
-@app.route('/', methods= METHOD)
-def extract():
-    extracted_skill = ResumeParser(request.form['desc'], skills_file=custom_skills).get_extracted_data()['skills']
-    return render_template('index.html', skills = extracted_skill)
+
 @app.route('/api', methods= METHOD)
 def JDParser():
     extracted_info = ResumeParser(request.json['desc'], skills_file=custom_skills).get_extracted_data()
@@ -20,10 +17,20 @@ def JDParser():
 @app.route('/extract')
 def before_extract():
     return render_template('extract.html')
+
 @app.route('/extract', methods= METHOD)
 def after_extract():
     extracted_info = job_desc_extractor(request.form['desc'])
-    return render_template('extract.html', extracted_info=extracted_info)
+    return render_template('extract.html',
+                           title =extracted_info['Title'],
+                           salary=extracted_info['Salary'],
+                           email=extracted_info['Email'],
+                           location=extracted_info['Location'],
+                           skills=extracted_info['Skills'],
+                           company_names=extracted_info['Company Name'],
+                           qualification=extracted_info['Qualification'],
+                           experience=extracted_info['Experience']
+                           )
 
 if __name__ == '__main__':
     app.run(host=HOST, port=PORT)
